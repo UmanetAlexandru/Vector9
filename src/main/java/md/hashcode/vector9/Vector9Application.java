d29b409781d7564ef3a7f79d4bcdf99187c5ff78
AlexandruUmanet <alexandru.umanet@gmail.com>
2026-03-11 22:15:24 +0200

    repo init

---
+++ src/main/java/md/hashcode/vector9/Vector9Application.java
@@ -0,0 +1,13 @@
+package md.hashcode.vector9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vector9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vector9Application.class, args);
+    }
+
+}
