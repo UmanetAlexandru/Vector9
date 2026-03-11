1bbf5a32eaf5c5700ddbaf9220fa293286ec41ba
AlexandruUmanet <alexandru.umanet@gmail.com>
2026-03-11 22:50:10 +0200

    refactor: use build metadata in health indicator
    - generate Spring Boot build info during the Maven build
    
    - inject BuildProperties into the custom health indicator
    
    - remove the hardcoded application version from health details

--- src/main/java/md/hashcode/vector9/config/HealthConfig.java
+++ src/main/java/md/hashcode/vector9/config/HealthConfig.java
@@ -2,16 +2,24 @@ package md.hashcode.vector9.config;
 
 import org.springframework.boot.health.contributor.Health;
 import org.springframework.boot.health.contributor.HealthIndicator;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
 @Component("vector9")
 public class HealthConfig implements HealthIndicator {
 
+    private final BuildProperties buildProperties;
+
+    public HealthConfig(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
     @Override
     public Health health() {
         return Health.up()
                 .withDetail("status", "Vector9 is running")
-                .withDetail("version", "1.0.0-SNAPSHOT")
+                .withDetail("name", buildProperties.getName())
+                .withDetail("version", buildProperties.getVersion())
                 .build();
     }
 }
